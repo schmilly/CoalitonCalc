873cbc1c92bf8f123990b8136c1dfa9c1a5d53da
Schmilly <william.f.s.bake@gmail.com>
2024-01-18 15:30:58 +0800

    Additonal work

--- Polling.js
+++ Polling.js
@@ -100,5 +100,104 @@ PollingData =
         "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.One, PrtyNme.Oth],
         "PartyData": [38, 29, 13, 3, 17],
         "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie,  PartyColor.Other]
-    }
+    },
+    {
+        "Date": "2024-02-25",
+        "Company": "InsightPolls",
+        "Level": "State - Queensland",
+        "Count": 6,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.KAP, PrtyNme.Oth],
+        "PartyData": [37, 26, 17, 6, 14],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-03-05",
+        "Company": "NationwideSurveys",
+        "Level": "Federal",
+        "Count": 5,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.One, PrtyNme.Oth],
+        "PartyData": [39, 30, 14, 5, 12],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.OneNation, PartyColor.Other]
+    },
+    {
+        "Date": "2024-03-10",
+        "Company": "MetroInsights",
+        "Level": "State - South Australia",
+        "Count": 4,
+        "PartyLabels": [PrtyNme.Lib, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.JLP, PrtyNme.OaI],
+        "PartyData": [32, 29, 16, 18, 5],
+        "PartyColor": [PartyColor.Liberal, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-03-15",
+        "Company": "PeopleVoice",
+        "Level": "Federal",
+        "Count": 7,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.KAP, PrtyNme.Oth],
+        "PartyData": [40, 27, 15, 7, 11],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-03-20",
+        "Company": "CityOpinions",
+        "Level": "State - Northern Territory",
+        "Count": 5,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.One, PrtyNme.Oth],
+        "PartyData": [35, 28, 18, 10, 9],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.OneNation, PartyColor.Other]
+    },
+    {
+        "Date": "2024-04-05",
+        "Company": "NationalTrends",
+        "Level": "Federal",
+        "Count": 6,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.KAP, PrtyNme.Oth],
+        "PartyData": [38, 29, 16, 6, 11],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-04-10",
+        "Company": "RegionInsights",
+        "Level": "State - Australian Capital Territory",
+        "Count": 4,
+        "PartyLabels": [PrtyNme.Lib, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.JLP, PrtyNme.OaI],
+        "PartyData": [33, 30, 15, 17, 5],
+        "PartyColor": [PartyColor.Liberal, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-04-15",
+        "Company": "SurveyInsights",
+        "Level": "Federal",
+        "Count": 7,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.One, PrtyNme.Oth],
+        "PartyData": [41, 26, 14, 8, 11],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.OneNation, PartyColor.Other]
+    },
+    {
+        "Date": "2024-04-20",
+        "Company": "VoiceOfPeople",
+        "Level": "State - South Australia",
+        "Count": 5,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.KAP, PrtyNme.Oth],
+        "PartyData": [36, 28, 17, 6, 13],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-05-05",
+        "Company": "AustralianInsights",
+        "Level": "Federal",
+        "Count": 6,
+        "PartyLabels": [PrtyNme.LaP, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.JLP, PrtyNme.OaI],
+        "PartyData": [37, 27, 16, 15, 5],
+        "PartyColor": [PartyColor.LiberalNat, PartyColor.Labor, PartyColor.Greens, PartyColor.JaquieLambie, PartyColor.Other]
+    },
+    {
+        "Date": "2024-05-10",
+        "Company": "StateVoice",
+        "Level": "State - Victoria",
+        "Count": 4,
+        "PartyLabels": [PrtyNme.Lib, PrtyNme.ALP, PrtyNme.Grn, PrtyNme.One, PrtyNme.Oth],
+        "PartyData": [34, 31, 14, 12, 9],
+        "PartyColor": [PartyColor.Liberal, PartyColor.Labor, PartyColor.Greens, PartyColor.OneNation, PartyColor.Other]
+    },
 ]
